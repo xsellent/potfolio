20aff445db287809a736915b894c550e8bd1d47a
xsellent <165433051+xsellent@users.noreply.github.com>
2024-03-29 23:21:37 +0000

    Creating an Azure static App

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  " I am a dedicated Computer Science student at Rivers State University with a keen interest in Azure cloud technology and backend development using Java";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Cloud computing",
+  "Backend development",
+  "Databases",
+  "Networking",
+  "DevOps"
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am deeply passionate about technology, particularly Azure cloud solutions and backend development using Java. With a passion for crafting scalable and efficient solutions, I specialize in harnessing the power of Azure to build robust cloud-based applications, alongside leveraging Java to create resilient backend systems. ";
 
 const About = () => {
   return (
